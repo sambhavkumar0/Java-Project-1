7504c7754e9c5f64118055736827a54873b37dd0
SAMBHAV KUMAR MOHANTY <108188982+sambhavkumar0@users.noreply.github.com>
2023-08-14 13:55:04 +0530

    Create main1.java
    Hello
    
    Co-Authored-By: Debasish Bhola <devop69@gmail.com>

---
+++ main1.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class main{
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n;
+        System.out.println("Enter a number between 0 and 9");
+        n=sc.nextInt();
+        switch(n) {
+            case 0:break;
+            case 1:break;
+            case 2:break;
+            case 3:break;
+            case 4:break;
+            case 5:break;
+            case 6:break;
+            case 7:break;
+            case 8:break;
+            case 9:break;
+            case 10:break;
+
+    }
+
+}
+
+}
+
