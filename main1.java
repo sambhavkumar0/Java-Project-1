c89c70fc0b7aec07c5ee96c25944c221bccedb92
Debasish Bhola <devop69@gmail.com>
2023-08-18 19:22:41 +0530

    Update main1.java
    Added Linear Search

--- main1.java
+++ main1.java
@@ -55,7 +55,19 @@ public class main1{
          double r= s.nextDouble();
          double  area=(22*r*r)/7 ;
          System.out.println("Area of Circle is: " + area);      
-   }
+   }    
+    public static int linearSearchGo(int arr[], int target){
+        if(arr.length == 0){
+            return -1;
+        }
+        for(int i=0; i<arr.length; i++){
+            int element = arr[i];
+            if(element == target){
+                return i;
+            }
+        }
+        return -1;
+    }
 
 }
 
