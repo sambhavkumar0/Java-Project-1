407dbca3e188a6d4d7b850e9647765d9b91c8335
Debasish Bhola <devop69@gmail.com>
2023-08-20 09:54:28 +0530

    Update main.java
    Added an order agnostic binary search

--- main1.java
+++ main1.java
@@ -218,6 +218,41 @@ public class main1{
         }
         return -1;
     }
+    public static int orderAgnostic(int arr[], int target){
+        int start = 0;
+        int end = arr.length-1;
+        //Finding wheather Ascending or Descending
+        
+        boolean isAsc;
+        if(arr[start]<arr[end]){
+            isAsc = true;
+        }else{
+            isAsc = false;
+        }
+
+        while(start<=end){
+            int mid = start +(end-start)/2;
+            
+            if(arr[mid] == target){
+                return mid;
+            }
+            if(isAsc){
+                if(target < arr[mid]){
+                    end = mid-1;
+                }
+                else if(target > arr[mid]){
+                    start = start+1;
+                }
+            } else{
+                if(target > arr[mid]){
+                    end = mid-1;
+                }else if(target < arr[mid]){
+                    start = mid+1;
+                }
+            }
+         }
+         return -1;
+    }
 
 }
 
