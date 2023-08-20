e1ea7bba8d210fcb4ed161b826d23337863063bb
Debasish Bhola <devop69@gmail.com>
2023-08-20 09:57:32 +0530

    Added Bubble Sort func
    Added bubble sort function

--- main1.java
+++ main1.java
@@ -253,6 +253,21 @@ public class main1{
          }
          return -1;
     }
+    public static void bubbleSort(int[] arr){
+
+        for(int i=0; i<arr.length;i++){
+            //for each step max item will come at the last respective index
+            for(int j=1; j<arr.length-i;j++){
+                //swap if the item is smaller than previous
+                if(arr[j]< arr[j-1]){
+                    //swap
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                }
+            }
+        }
+    }
 
 }
 
